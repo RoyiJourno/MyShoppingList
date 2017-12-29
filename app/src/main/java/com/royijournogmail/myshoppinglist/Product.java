e3100743d86ded0dd5adb48e384189d725852dc4
RoyiJourno <royijourno@gmail.com>
2017-12-29 08:59:12 +0200

    a

--- app/src/main/java/com/royijournogmail/myshoppinglist/Product.java
+++ app/src/main/java/com/royijournogmail/myshoppinglist/Product.java
@@ -15,8 +15,8 @@ public class Product {
         this.p_id=p_id;
         this.p_name = p_name;
         this.p_amount= p_amount;
-        this.p_photoUrl= p_photoUrl;
-        this.p_description= p_description;
+        this.p_photoUrl = p_photoUrl;
+        this.p_description = p_description;
     }
 }
 
