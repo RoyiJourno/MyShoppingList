88cda7a86b78bb81de85ceb8b3595ec50fe035df
RoyiJourno <royijourno@gmail.com>
2017-12-31 14:54:17 +0200

    fs

--- app/src/main/java/com/royijournogmail/myshoppinglist/HomePage.java
+++ app/src/main/java/com/royijournogmail/myshoppinglist/HomePage.java
@@ -1,7 +1,10 @@
 package com.royijournogmail.myshoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
@@ -12,5 +15,19 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
         TextView homeEmail = (TextView)findViewById(R.id.homeEmail);
         homeEmail.setText(getIntent().getExtras().getString("Email"));
+
+        final Button addProduct = (Button)findViewById(R.id.addProduct);
+        Button chooseItem = (Button)findViewById(R.id.chooseItem);
+        Button previousList = (Button)findViewById(R.id.previousList);
+        Button statistic = (Button)findViewById(R.id.statistic);
+
+        addProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this,AddProduct.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
